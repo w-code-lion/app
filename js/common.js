32b948f218c203bd905371bcc83be657384a1c0b
w-code-lion <w.code.lion@gmail.com>
2020-10-11 06:19:36 +0300

    Initial commit

---
+++ js/common.js
@@ -0,0 +1,13 @@
+$(function() {
+
+   $(document).ready(function(){
+      $('.hdr__slider').slick({
+         dots: true,
+         infinite: false,
+         autoplay:true,
+         speed:800
+      });
+    });
+
+});
+ 
